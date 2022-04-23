7f7024f19b079ae0e87acc24db94c604d480a064
Rachel Johnson <Rachel.Johnson.2000@live.com>
2022-04-23 16:06:01 -0500

    Create movie template

---
+++ my-app/src/movies.jsx
@@ -0,0 +1,75 @@
+import React, {useState} from 'react'
+
+const movies = () => {
+    const [movie, setMovie] = useState([
+        {
+            "Title": "Space Jam",
+            "Poster": "",
+            "Times": "",
+            "Seats Avalaible": ""
+        },
+        {
+            "Title": "Space Jam Odyssey",
+            "Poster": "",
+            "Times": "",
+            "Seats Avalaible": ""
+        },
+        {
+            "Title": "Space Ball",
+            "Poster": "",
+            "Times": "",
+            "Seats Avalaible": ""
+        },
+        {
+            "Title": "Space Odyssey",
+            "Poster": "",
+            "Times": "",
+            "Seats Avalaible": ""
+        },
+        {
+            "Title": "Mars Attack!",
+            "Poster": "",
+            "Times": "",
+            "Seats Avalaible": ""
+        },
+        {
+            "Title": "The Martian",
+            "Poster": "",
+            "Times": "",
+            "Seats Avalaible": ""
+        },
+        {
+            "Title": "Interstellar",
+            "Poster": "",
+            "Times": "",
+            "Seats Avalaible": ""
+        },
+        {
+            "Title": "Planet 51",
+            "Poster": "",
+            "Times": "",
+            "Seats Avalaible": ""
+        },
+        {
+            "Title": "Gravity",
+            "Poster": "",
+            "Times": "",
+            "Seats Avalaible": ""
+        },
+        {
+            "Title": "Guardians of the Galaxy",
+            "Poster": "",
+            "Times": "",
+            "Seats Avalaible": ""
+        },
+        {
+            "Title": "Alien",
+            "Poster": "",
+            "Times": "",
+            "Seats Avalaible": ""
+        }
+    ]);
+    
+};
+
+export default movies;
